10aa90da31d27ce0166260a6d633a3ac94f43f32
Abomide Oluwaseye <142733387+Teleiosite@users.noreply.github.com>
2026-04-11 16:12:57 +0100

    Add intro.js assignment

---
+++ intro.js
@@ -0,0 +1,9 @@
+// intro.js - Personal Introduction
+
+const name = "Abose";
+const age = 20;
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
